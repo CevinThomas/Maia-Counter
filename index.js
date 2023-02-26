04357f48b59568cd582f3c31c1d577c1b61edbf9
Cevin Thomas <cevin.thomas.ny@gmail.com>
2023-02-26 23:39:00 +0100

    init

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const cheerio = require("cheerio");
+const axios = require("axios");
+const app = express();
+const port = 3000;
+const organizationId = "76974546";
+
+app.get("/", (req, res) => {
+  axios
+    .get(
+      "https://www.linkedin.com/pages-extensions/FollowCompany?id=76974546&counter=bottom"
+    )
+    .then((response) => {
+      const htmlData = response.data;
+      const $ = cheerio.load(htmlData);
+
+      const followerElement = $(".follower-count")[0].children[0].data;
+
+      const followerValue = followerElement.replace(",", "");
+
+      res.send({ number: +followerValue });
+    })
+    .then(() => res.send({ number: 0 }));
+});
+/** app.get("/auth", (req, res) => {
+  const code = req.query.code;
+  const authUrl = `https://www.linkedin.com/oauth/v2/accessToken`;
+
+  console.log("CODE: ", code);
+  const data = {
+    code: code,
+    grant_type: "authorization_code",
+    client_id: clientId,
+    client_secret: clientSecret,
+    redirect_uri: "https://localhost:3000/auth",
+  };
+  const config = {
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+  };
+  axios.post(authUrl, data, config).then().catch();
+
+  res.send("HELLO");
+});
+*/
+
+app.listen(port, () => {
+  console.log("LISTENING");
+});
